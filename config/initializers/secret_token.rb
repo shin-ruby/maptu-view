8362faff01fe6c1298109ae4dfda7fd137c5df2a
shin <shin.php@gmail.com>
2012-06-24 13:06:50 +0800

    'first_commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaptuView::Application.config.secret_token = '660949fd968a3f20a4d5a0e9e703c47c7b74cbf0fa786ffd0e140ea07cdff027014482f88e84db1f13cfe7517a395bd731b68d1ea444d31e4435fbf5b3eb4f6a'
